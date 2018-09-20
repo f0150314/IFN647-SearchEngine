9d945c71bbe113a7f1c1300e2386ca82d8a8ee81
n9861718 <n9861718@VDI-VL04-016>
2018-09-20 17:17:10 +1000

    First commit (Completing document indexing and preprocessing)

---
+++ SearchEngine/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace SearchEngine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBuildIndexDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.BuildIndexButton = new System.Windows.Forms.Button();
+            this.DirectoryPathLabel = new System.Windows.Forms.Label();
+            this.folderCollectionDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.CollectionButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BuildIndexButton
+            // 
+            this.BuildIndexButton.Location = new System.Drawing.Point(12, 12);
+            this.BuildIndexButton.Name = "BuildIndexButton";
+            this.BuildIndexButton.Size = new System.Drawing.Size(203, 23);
+            this.BuildIndexButton.TabIndex = 6;
+            this.BuildIndexButton.Text = "Select the Index Directory";
+            this.BuildIndexButton.UseVisualStyleBackColor = true;
+            this.BuildIndexButton.Click += new System.EventHandler(this.BuildIndexButton_Click);
+            // 
+            // DirectoryPathLabel
+            // 
+            this.DirectoryPathLabel.Location = new System.Drawing.Point(236, 12);
+            this.DirectoryPathLabel.Name = "DirectoryPathLabel";
+            this.DirectoryPathLabel.Size = new System.Drawing.Size(223, 23);
+            this.DirectoryPathLabel.TabIndex = 7;
+            this.DirectoryPathLabel.Text = "IndexDirectoryPath";
+            this.DirectoryPathLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CollectionButton
+            // 
+            this.CollectionButton.Location = new System.Drawing.Point(12, 41);
+            this.CollectionButton.Name = "CollectionButton";
+            this.CollectionButton.Size = new System.Drawing.Size(203, 23);
+            this.CollectionButton.TabIndex = 8;
+            this.CollectionButton.Text = "Source collection path";
+            this.CollectionButton.UseVisualStyleBackColor = true;
+            this.CollectionButton.Click += new System.EventHandler(this.CollectionButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 477);
+            this.Controls.Add(this.CollectionButton);
+            this.Controls.Add(this.DirectoryPathLabel);
+            this.Controls.Add(this.BuildIndexButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBuildIndexDialog;
+        private System.Windows.Forms.Button BuildIndexButton;
+        private System.Windows.Forms.Label DirectoryPathLabel;
+        private System.Windows.Forms.FolderBrowserDialog folderCollectionDialog;
+        private System.Windows.Forms.Button CollectionButton;
+    }
+}
+
---
+++ SearchEngine/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Lucene.Net.Store;
+using Lucene.Net.Index;
+using Lucene.Net.Documents;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+
+namespace SearchEngine
+{
+    public partial class Form1 : Form
+    {
+        System.IO.StreamReader reader;
+        Directory luceneIndexDirectory;
+        Analyzer analyzer;
+        IndexWriter writer;
+        Stopwatch stopwatch;
+
+        const Lucene.Net.Util.Version VERSION = Lucene.Net.Util.Version.LUCENE_30;
+        const string DOC_ID = "DocID";
+        const string TITLE = "Title";
+        const string AUTHOR = "Author";
+        const string BIBLIO_INFO = "BiblioInfo";
+        const string ABSTRACT = "Abstract";
+
+        string[] delimiters = { ".I ", ".T", ".A", ".B", ".W" };
+        string indexPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            reader = null;
+            luceneIndexDirectory = null;
+            analyzer = null;
+            writer = null;
+            stopwatch = null;
+        }
+
+        private void BuildIndexButton_Click(object sender, EventArgs e)
+        {
+            if (folderBuildIndexDialog.ShowDialog() == DialogResult.OK)
+            {
+                indexPath = folderBuildIndexDialog.SelectedPath;
+                DirectoryPathLabel.Text = indexPath;
+                OpenIndex(indexPath);
+            }
+        }
+        private void OpenIndex(string indexPath)
+        {
+            luceneIndexDirectory = FSDirectory.Open(indexPath);
+            analyzer = new StandardAnalyzer(VERSION);
+            IndexWriter.MaxFieldLength mfl = new IndexWriter.MaxFieldLength(IndexWriter.DEFAULT_MAX_FIELD_LENGTH);
+            writer = new IndexWriter(luceneIndexDirectory, analyzer, true, mfl);
+        }
+
+        private void CollectionButton_Click(object sender, EventArgs e)
+        {
+            if (folderCollectionDialog.ShowDialog() == DialogResult.OK)
+            {
+                string collectionPath = folderCollectionDialog.SelectedPath;
+                WalkDirectoryTree(collectionPath);
+                CleanUpIndexer();
+            }
+        }
+        private void WalkDirectoryTree(string collectionPath)
+        {
+            System.IO.DirectoryInfo root = new System.IO.DirectoryInfo(collectionPath);
+            System.IO.FileInfo[] files = null;
+            System.IO.DirectoryInfo[] subDirs = null;
+
+            // First, process all the files directly under this folder 
+            try
+            {
+                files = root.GetFiles("*.*");
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            if (files != null)
+            {
+                stopwatch = new Stopwatch();
+                stopwatch.Restart();
+                // Process every file
+                foreach (System.IO.FileInfo fi in files)
+                {
+                    string name = fi.FullName;
+                    ReadDocuments(name);
+                }
+                reader.Close();
+
+                // Now find all the subdirectories under this directory.
+                subDirs = root.GetDirectories();
+
+                // Resursive call for each subdirectory.
+                foreach (System.IO.DirectoryInfo dirInfo in subDirs)
+                {
+                    string name = dirInfo.FullName;
+                    WalkDirectoryTree(name);
+                }
+                stopwatch.Stop();
+            }
+            var elapsed = stopwatch.Elapsed;
+            MessageBox.Show($"Indexing time: {elapsed} seconds");
+        }
+        private void ReadDocuments(string name)
+        {
+            reader = new System.IO.StreamReader(name);
+            string text = reader.ReadToEnd();
+            string[] docInfo = ProcessedDocuments(text);
+            IndexText(docInfo);
+        }
+        private string[] ProcessedDocuments(string text)
+        {
+            string[] docInfo = text.ToLower().Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            docInfo[4] = docInfo[4].Remove(0, docInfo[1].Length);
+            return docInfo;
+        }
+        private void IndexText(string[] docInfo)
+        {
+            Document doc = new Document();
+            doc.Add(new Field(DOC_ID, docInfo[0], Field.Store.YES, Field.Index.NO, Field.TermVector.NO));
+            doc.Add(new Field(TITLE, docInfo[1], Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            doc.Add(new Field(AUTHOR, docInfo[2], Field.Store.YES, Field.Index.NOT_ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            doc.Add(new Field(BIBLIO_INFO, docInfo[3], Field.Store.YES, Field.Index.NOT_ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            doc.Add(new Field(ABSTRACT, docInfo[4], Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            writer.AddDocument(doc);
+        }
+        private void CleanUpIndexer()
+        {
+            writer.Optimize();
+            writer.Flush(true, true, true);
+            writer.Dispose();
+        }
+    }
+}
