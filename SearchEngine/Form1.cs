a553b6d9d642a6542789d8964e3b663ab5b31451
n9861718 <n9861718@VDI-VL04-016>
2018-09-22 13:50:22 +1000

    Remove previous functions from Form1.cs and create a indexClass for indexing functions.

--- SearchEngine/Form1.cs
+++ SearchEngine/Form1.cs
@@ -7,61 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Diagnostics;
-using Lucene.Net.Store;
-using Lucene.Net.Index;
-using Lucene.Net.Documents;
-using Lucene.Net.Analysis;
-using Lucene.Net.Analysis.Standard;
-using Lucene.Net.Analysis.Snowball;
 
 namespace SearchEngine
 {
     public partial class Form1 : Form
     {
-        System.IO.StreamReader reader;
-        Directory luceneIndexDirectory;
-        Analyzer analyzer;
-        IndexWriter writer;
-        Stopwatch stopwatch;
-
-        const Lucene.Net.Util.Version VERSION = Lucene.Net.Util.Version.LUCENE_30;
-        const string DOC_ID = "DocID";
-        const string TITLE = "Title";
-        const string AUTHOR = "Author";
-        const string BIBLIO_INFO = "BiblioInfo";
-        const string ABSTRACT = "Abstract";
-
-        
-        string indexPath;
+        IndexingClass indexing;
 
         public Form1()
         {
             InitializeComponent();
-            reader = null;
-            luceneIndexDirectory = null;
-            analyzer = null;
-            writer = null;
-            stopwatch = null;
+            indexing = new IndexingClass();
         }
 
         private void BuildIndexButton_Click(object sender, EventArgs e)
         {
             if (folderBuildIndexDialog.ShowDialog() == DialogResult.OK)
             {
-                indexPath = folderBuildIndexDialog.SelectedPath;
-                DirectoryPathLabel.Text = indexPath;
-                OpenIndex(indexPath);
+                DirectoryPathLabel.Text = folderBuildIndexDialog.SelectedPath;                
+                indexing.OpenIndex(DirectoryPathLabel.Text);
             }
-        }
-
-        // Open index and initialize analyzer and indexWriter
-        private void OpenIndex(string indexPath)
-        {
-            luceneIndexDirectory = FSDirectory.Open(indexPath);
-            analyzer = new StandardAnalyzer(VERSION);
-            IndexWriter.MaxFieldLength mfl = new IndexWriter.MaxFieldLength(IndexWriter.DEFAULT_MAX_FIELD_LENGTH);
-            writer = new IndexWriter(luceneIndexDirectory, analyzer, true, mfl);
         }
 
         private void CollectionButton_Click(object sender, EventArgs e)
@@ -69,85 +34,9 @@ namespace SearchEngine
             if (folderCollectionDialog.ShowDialog() == DialogResult.OK)
             {
                 string collectionPath = folderCollectionDialog.SelectedPath;
-                WalkDirectoryTree(collectionPath);
-                CleanUpIndexer();
+                indexing.WalkDirectoryTree(collectionPath);
+                indexing.CleanUpIndexer();
             }
-        }
-
-        // Read through all files
-        private void WalkDirectoryTree(string collectionPath)
-        {
-            System.IO.DirectoryInfo root = new System.IO.DirectoryInfo(collectionPath);
-            System.IO.FileInfo[] files = null;
-            System.IO.DirectoryInfo[] subDirs = null;
-
-            // First, process all the files directly under this folder 
-            try
-            {
-                files = root.GetFiles("*.*");
-            }
-            catch (UnauthorizedAccessException e)
-            {
-                MessageBox.Show(e.Message);
-            }
-
-            catch (System.IO.DirectoryNotFoundException e)
-            {
-                MessageBox.Show(e.Message);
-            }
-            if (files != null)
-            {
-                stopwatch = new Stopwatch();
-                stopwatch.Restart();
-                // Process every file
-                foreach (System.IO.FileInfo fi in files)
-                {
-                    string name = fi.FullName;
-                    IndexingDocuments(name);
-                }
-                reader.Close();
-
-                // Now find all the subdirectories under this directory.
-                subDirs = root.GetDirectories();
-
-                // Resursive call for each subdirectory.
-                foreach (System.IO.DirectoryInfo dirInfo in subDirs)
-                {
-                    string name = dirInfo.FullName;
-                    WalkDirectoryTree(name);
-                }
-                stopwatch.Stop();
-            }
-            var elapsed = stopwatch.Elapsed;
-            MessageBox.Show($"Indexing time: {elapsed} seconds");
-        }
-
-        // Read and preprocess documents
-        private void IndexingDocuments(string name)
-        {
-            // Preprocessing document (remove abstract error)
-            reader = new System.IO.StreamReader(name);
-            string text = reader.ReadToEnd();
-            string[] delimiters = { ".I ", ".T", ".A", ".B", ".W" };
-            string[] docInfo = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
-            docInfo[4] = docInfo[4].Remove(0, docInfo[1].Length);
-
-            // Creating Index for four different fields
-            Document doc = new Document();
-            doc.Add(new Field(DOC_ID, docInfo[0], Field.Store.YES, Field.Index.NO, Field.TermVector.NO));
-            doc.Add(new Field(TITLE, docInfo[1], Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
-            doc.Add(new Field(AUTHOR, docInfo[2], Field.Store.YES, Field.Index.NOT_ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
-            doc.Add(new Field(BIBLIO_INFO, docInfo[3], Field.Store.YES, Field.Index.NOT_ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
-            doc.Add(new Field(ABSTRACT, docInfo[4], Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
-            writer.AddDocument(doc);
-        }
-      
-        // Clean up Indexer
-        private void CleanUpIndexer()
-        {
-            writer.Optimize();
-            writer.Flush(true, true, true);
-            writer.Dispose();
         }
     }
 }
---
+++ SearchEngine/IndexingClass.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Lucene.Net.Store;
+using Lucene.Net.Index;
+using Lucene.Net.Documents;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Analysis.Snowball;
+
+namespace SearchEngine
+{
+    class IndexingClass
+    {
+        System.IO.StreamReader reader;
+        Directory luceneIndexDirectory;
+        Analyzer analyzer;
+        IndexWriter writer;
+        Stopwatch stopwatch;
+
+        const Lucene.Net.Util.Version VERSION = Lucene.Net.Util.Version.LUCENE_30;
+        const string DOC_ID = "DocID";
+        const string TITLE = "Title";
+        const string AUTHOR = "Author";
+        const string BIBLIO_INFO = "BiblioInfo";
+        const string ABSTRACT = "Abstract";
+
+        public IndexingClass()
+        {
+            reader = null;
+            luceneIndexDirectory = null;
+            analyzer = null;
+            writer = null;
+            stopwatch = null;
+        }
+
+        // Open index and initialize analyzer and indexWriter
+        public void OpenIndex(string DirectoryPath)
+        {
+            luceneIndexDirectory = FSDirectory.Open(DirectoryPath);
+            analyzer = new StandardAnalyzer(VERSION);
+            IndexWriter.MaxFieldLength mfl = new IndexWriter.MaxFieldLength(IndexWriter.DEFAULT_MAX_FIELD_LENGTH);
+            writer = new IndexWriter(luceneIndexDirectory, analyzer, true, mfl);
+        }
+
+        // Read through all files
+        public void WalkDirectoryTree(string collectionPath)
+        {
+            System.IO.DirectoryInfo root = new System.IO.DirectoryInfo(collectionPath);
+            System.IO.FileInfo[] files = null;
+            System.IO.DirectoryInfo[] subDirs = null;
+
+            // First, process all the files directly under this folder 
+            try
+            {
+                files = root.GetFiles("*.*");
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            if (files != null)
+            {
+                stopwatch = new Stopwatch();
+                stopwatch.Restart();
+                // Process every file
+                foreach (System.IO.FileInfo fi in files)
+                {
+                    string name = fi.FullName;
+                    IndexingDocuments(name);
+                }
+                reader.Close();
+
+                // Now find all the subdirectories under this directory.
+                subDirs = root.GetDirectories();
+
+                // Resursive call for each subdirectory.
+                foreach (System.IO.DirectoryInfo dirInfo in subDirs)
+                {
+                    string name = dirInfo.FullName;
+                    WalkDirectoryTree(name);
+                }
+                stopwatch.Stop();
+            }
+            var elapsed = stopwatch.Elapsed;
+            MessageBox.Show($"Indexing time: {elapsed} seconds");
+        }
+
+        // Preprocess documents and add to index
+        public void IndexingDocuments(string name)
+        {
+            // Preprocessing document (remove abstract error)
+            reader = new System.IO.StreamReader(name);
+            string text = reader.ReadToEnd();
+            string[] delimiters = { ".I ", ".T", ".A", ".B", ".W" };
+            string[] docInfo = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            docInfo[4] = docInfo[4].Remove(0, docInfo[1].Length);
+
+            // Creating Index for four different fields
+            Document doc = new Document();
+            doc.Add(new Field(DOC_ID, docInfo[0], Field.Store.YES, Field.Index.NO, Field.TermVector.NO));
+            doc.Add(new Field(TITLE, docInfo[1], Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            doc.Add(new Field(AUTHOR, docInfo[2], Field.Store.YES, Field.Index.NOT_ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            doc.Add(new Field(BIBLIO_INFO, docInfo[3], Field.Store.YES, Field.Index.NOT_ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            doc.Add(new Field(ABSTRACT, docInfo[4], Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            writer.AddDocument(doc);
+        }
+
+        // Clean up Indexer
+        public void CleanUpIndexer()
+        {
+            writer.Optimize();
+            writer.Flush(true, true, true);
+            writer.Dispose();
+        }
+    }
+}
